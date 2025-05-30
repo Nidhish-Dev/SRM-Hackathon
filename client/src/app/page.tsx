37c8e6ade31df00d68da579e1cdca53882591af3
Nidhish Rathore <codenidhish07@gmail.com>
2025-05-30 14:36:06 +0530

    Added Initial Files

---
+++ client/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+    <p className="font-extralight"> Whereas recognition of the inherent dignity</p>
+    </>
+  );
+}
